abb790f410bfd4f9cfb0a6b4a2bf74785aa7e576
mfrt <ps.fx@ya.ru>
2021-10-29 09:21:49 +0300

    update

--- LineDriving/main.py
+++ LineDriving/main.py
@@ -17,7 +17,7 @@ MIN_LIGHT = 12
 
 # FNS
 # Езда по перекрёстка
-def goto_perek(speed=600, a=0.5, b=4, c=0.1, d=15, dif=0):
+def goto_perek(speed=600, a=0.5, l=0.4, b=4, c=0.1, d=15, dif=0, perek=False):
     global MIN_LIGHT
     ml = Motor(Port.B)
     mr = Motor(Port.C)
@@ -26,19 +26,20 @@ def goto_perek(speed=600, a=0.5, b=4, c=0.1, d=15, dif=0):
     integral = 0
     last_error = 0
     while True:
-        #if sl.reflection() <= MIN_LIGHT and sr.reflection() <= MIN_LIGHT:
-        #    ml.hold()
-        #    mr.hold()
-        #    return True
+        if perek and sl.reflection() <= MIN_LIGHT and sr.reflection() <= MIN_LIGHT:
+            ml.brake()
+            mr.brake()
+            break
         ref = sl.reflection() - sr.reflection() + dif
         integral = integral*a + ref
         new_error = ref - last_error
         last_error = ref
-        angle = ref*b + integral*c + new_error*d
+        angle = ref*l + (b*ref)**3 + integral*c + new_error*d
         ml.run(speed+angle)
         mr.run(speed-angle)
+    return True
 
 if __name__ == "__main__":
-    goto_perek(speed=600, a=0.5, b=4, c=0.1, d=15, dif=0)
+    goto_perek(speed=400, a=0.8, l=1.7, b=0.003, c=0.25, d=17.5, dif=0)
 
 
